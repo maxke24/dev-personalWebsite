25084dcdbeae90959e528b835f8b656de2d15392
master <jellemax08@hotmail.com>
2021-04-08 21:24:00 +0200

    Adding service worker and localForage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,25 @@
 "use strict";
 
 $(document).ready(function () {
-    $("#terminal").load("terminal.html", ()=>{
-        $.getScript("assets/js/navHandling.js");
-        $.getScript("assets/js/terminalFunctionality.js");
-        $.getScript("assets/js/terminal.js");
-        $.getScript("assets/js/dragHandler.js");
+	registerServiceWorker();
 
-    });
-
+	$("#terminal").load("terminal.html", () => {
+		$.getScript("assets/js/navHandling.js");
+		$.getScript("assets/js/terminalFunctionality.js");
+		$.getScript("assets/js/terminal.js");
+		$.getScript("assets/js/dragHandler.js");
+	});
 });
+
+function registerServiceWorker() {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker
+			.register("/sw.js")
+			.then((res) =>
+				console.log(
+					`successfully registered serviceWorker with scope ${res.scope}`
+				)
+			)
+			.catch((err) => console.error("Error installing serviceWorker", err));
+	}
+}
--- assets/js/terminalFunctionality.js
+++ assets/js/terminalFunctionality.js
@@ -1,103 +1,121 @@
-const commands = ["help", "clear", "exit", "enable dark mode", "enable light mode", "disable dark mode", "disable light mode", "load color palette", "close color palette"];
+"use strict";
+
+const commands = [
+	"help",
+	"clear",
+	"exit",
+	"enable dark mode",
+	"enable light mode",
+	"disable dark mode",
+	"disable light mode",
+	"load color palette",
+	"close color palette",
+];
 
 /* --Detecting keypress for opening terminal-- */
 document.addEventListener("keyup", (event) => {
-    const keyname = event.key;
-    if (keyname === "²") {
-        toggleVisibility();
-        clearOutput();
-    }
+	const keyName = event.key;
+	if (keyName === "²") {
+		toggleVisibility();
+		clearOutput();
+	}
 });
 
 /* --Toggling the show and hide of the terminal-- */
 function toggleVisibility() {
-    terminal.classList.toggle("hidden");
-    inputBox.focus();
+	terminal.classList.toggle("hidden");
+	inputBox.focus();
 }
 
 /* --closing and clearing the terminal-- */
 function closeTerminal() {
-    clearOutput();
-    toggleVisibility();
+	clearOutput();
+	toggleVisibility();
 }
 
 /* --Let user put terminal fullscreen-- */
 function fullScreen() {
-    terminal.style.width = "100%";
-    terminal.style.height = "100%";
+	terminal.style.width = "100%";
+	terminal.style.height = "100%";
 }
 
 /* --Making typed input show as a stacktrace-- */
 function submitCommand() {
-    let command = inputBox.value;
-    createTag(command);
-    runCommand(command);
-    inputBox.value = "";
+	let command = inputBox.value;
+	createTag(command);
+	runCommand(command);
+	inputBox.value = "";
 }
 
 function runCommand(command) {
-    if (commands.includes(command)) {
-        switch (command) {
-            case "help":
-                showAllCommands();
-                break;
-            case "clear":
-                clearOutput();
-                break;
-            case "exit":
-                closeTerminal();
-                break;
-            case "enable light mode":
-            case "disable dark mode":
-                lightMode();
-                createTag("Light mode is enabled!", true);
-                break;
-            case "disable light mode":
-            case "enable dark mode":
-                darkMode();
-                createTag("Dark mode is enabled!", true);
-                break;
-            case "load color palette":
-                loadColors();
-                break;
-            case "close color palette":
-                closeColors();
-                break;
-            default:
-                inputBox.value = "command recognized";
-                createTag("command recognized", true);
-                break;
-        }
-    } else {
-        createTag("Command not recognized, please type help to show available commands", true);
-    }
+	if (commands.includes(command)) {
+		switch (command) {
+			case "help":
+				showAllCommands();
+				break;
+			case "clear":
+				clearOutput();
+				break;
+			case "exit":
+				closeTerminal();
+				break;
+			case "enable light mode":
+			case "disable dark mode":
+				lightMode();
+				localforage.setItem("mode", "lightMode");
+				createTag("Light mode is enabled!", true);
+				break;
+			case "disable light mode":
+			case "enable dark mode":
+				darkMode();
+				localforage.setItem("mode", "darkMode");
+				createTag("Dark mode is enabled!", true);
+				break;
+			case "load color palette":
+				loadColors();
+				break;
+			case "close color palette":
+				closeColors();
+				break;
+			default:
+				inputBox.value = "command recognized";
+				createTag("command recognized", true);
+				break;
+		}
+	} else {
+		createTag(
+			"Command not recognized, please type help to show available commands",
+			true
+		);
+	}
 }
 
 function showAllCommands() {
-    let list = document.createElement("ul");
-    commands.forEach((command) => {
-        let li = document.createElement("li");
-        let pTag = document.createElement("p");
-        pTag.innerHTML = `- ${command}`;
-        li.appendChild(pTag);
-        list.appendChild(li);
-    });
-    lines.appendChild(list);
+	let list = document.createElement("ul");
+	commands.forEach((command) => {
+		let li = document.createElement("li");
+		let pTag = document.createElement("p");
+		pTag.innerHTML = `- ${command}`;
+		li.appendChild(pTag);
+		list.appendChild(li);
+	});
+	lines.appendChild(list);
 }
 
 function clearOutput() {
-    lines.innerHTML = "";
-    inputBox.value = "";
-
+	lines.innerHTML = "";
+	inputBox.value = "";
 }
 
 function createTag(text, output = false) {
-    let ptag = document.createElement("p");
-    if (output) {
-        ptag.innerHTML = text;
-    } else {
-        ptag.innerHTML = `<span class="user">root@dev.jellemax.be</span>:<span class="tilde">~</span>$ ` + text;
-    }
-    ptag.setAttribute("class", "terminal_type");
-    lines.appendChild(ptag);
+	let ptag = document.createElement("p");
+	if (output) {
+		ptag.innerHTML = text;
+	} else {
+		ptag.innerHTML =
+			`<span class="user">root@dev.jellemax.be</span>:<span class="tilde">~</span>$ ` +
+			text;
+	}
+	ptag.setAttribute("class", "terminal_type");
+	lines.appendChild(ptag);
 }
